01016667c59255d68e4101529c44babba9bef2ca
Marjan-001 <marjan199721@gmail.com>
2023-01-18 00:21:51 +0600

    post like state change dynamically

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -2,12 +2,19 @@ import { MoreVert } from '@material-ui/icons';
 import React from 'react';
 import "./post.css";
 import { Users } from "../../fakeData"
+import { useState } from 'react';
 
 const Post = ({ post }) => {
     // const user = Users.filter(u => u.id === 1);
     // console.log(user);
     // console.log(post);
-    const { desc, photo, date, id, like, comment, userId } = post
+    const { desc, photo, date, id, comment, userId } = post
+    const [like, setLike] = useState(post.like);
+    const [isLiked, setIsLiked] = useState(false);
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1)
+        setIsLiked(!isLiked)
+    }
     return (
         <div className='post'>
             <div className="postWrapper">
@@ -27,8 +34,12 @@ const Post = ({ post }) => {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img src="assets/like.png" alt="" className="likeIcon" />
-                        <img src="assets/heart.png" alt="" className="likeIcon" />
+                        <img src="assets/like.png" alt=""
+                            onClick={likeHandler}
+                            className="likeIcon" />
+                        <img src="assets/heart.png" alt=""
+                            onClick={likeHandler}
+                            className="likeIcon" />
                         <span className="postLikeCounter">{like} people like this</span>
                     </div>
                     <div className="postBottomRight">
